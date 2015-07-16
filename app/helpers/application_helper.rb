9c5430894d2e84b57a2aec4044f720a822492adf
Dennis Tusk <dtusk@users.noreply.github.com>
2015-07-16 20:37:33 +0200

    Update application_helper.rb
    I really don't know why I haven't done that before ..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,14 @@ module ApplicationHelper
   # Returns true or false
   #
   def user_signed_in?
-    User::MyBBSessionTable.exists? sid: cookies[:sid] if cookies[:sid]
+    @user_signed_in ||= User::MyBBSessionTable.exists? sid: cookies[:sid] if cookies[:sid]
   end
 
   #
   # Returns currently logged in member record
   #
   def current_user
-    User::MyBBSessionTable.find(cookies[:sid]).user if cookies[:sid]
+    @user ||= User::MyBBSessionTable.find(cookies[:sid]).user if cookies[:sid]
   end
  
 end
